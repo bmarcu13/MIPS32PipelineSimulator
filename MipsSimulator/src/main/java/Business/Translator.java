c0c5474933f0393ae69fa87e31ce1efb891375f9
Bogdan Marcu <bogdanmarcu2003@gmail.com>
2024-12-05 17:05:36 +0200

    Break translateInstruction into separate function for each instruction class

--- MipsSimulator/src/main/java/Business/Translator.java
+++ MipsSimulator/src/main/java/Business/Translator.java
@@ -76,41 +76,12 @@ public class Translator {
         }
 
         switch (instructionId) {
-            case "j": {
-                Matcher matcher = jumpAddrPattern.matcher(instruction);
-                if (!matcher.matches()) {
-                    throw new TranslationError("Invalid address: " + result[1]);
-                }
-                int address = Integer.parseInt(matcher.group(1), 2);
-                return (opCode << 26) | address;
-            }
+            case "j":
+                return parseJ(instruction, opCode, result[1]);
 
-            case "add", "sub", "and", "or", "xor": {
-                String[] tokens = result[1].split("\\s*,\\s*");
-                int[] operands = new int[3];
-                for(int i = 0; i < tokens.length; i++) {
-                    Matcher matcher = registerOperandPattern.matcher(tokens[i]);
-                    if(!matcher.matches()) {
-                        throw new TranslationError("Unexpected token " + tokens[i]);
-                    }
-                    if(i < 3){
-                        int op = Integer.parseInt(tokens[i].substring(1));
-                        if(op > 31) {
-                            throw new TranslationError("Register index out of bounds: " + tokens[i]);
-                        }
-                        operands[i] = op;
-                    }
-                }
-                if(tokens.length != 3) {
-                    throw new TranslationError(
-                            String.format("Instruction %s expects 3 operands. Got %d instead.", instructionId, tokens.length)
-                    );
-                }
-                if(operands[0] == 0) {
-                    throw new TranslationError("Use of $0 as destination is forbidden");
-                }
-                return (opCode << 26) | (operands[1] << 21) | (operands[2] << 16) | (operands[0] << 11) | (instructionFunc.get(instructionId));
-            }
+            case "add", "sub", "and", "or", "xor":
+                return parseRType(instructionId, opCode, result[1]);
+
             case "sll", "srl":
                 break;
             case "addi", "subi", "lw", "sw", "bgtz", "be", "bne":
@@ -118,5 +89,77 @@ public class Translator {
         }
 
         throw new TranslationError("Unexpected translation error");
+    }
+
+    private static int parseJ(String instruction, int opCode, String instrBody) throws TranslationError {
+        Matcher matcher = jumpAddrPattern.matcher(instruction);
+        if (!matcher.matches()) {
+            throw new TranslationError("Invalid jump address: " + instrBody);
+        }
+        int address = Integer.parseInt(matcher.group(1), 2);
+        return (opCode << 26) | address;
+    }
+
+    private static int parseRType(String instructionId, int opCode, String instrBody) throws TranslationError {
+        String[] tokens = instrBody.split("\\s*,\\s*"); // split the remaining instruction tokens by ,
+        int[] operands = new int[3];
+        for(int i = 0; i < tokens.length; i++) {
+            Matcher matcher = registerOperandPattern.matcher(tokens[i]); // try to match the token as a valid
+            // operand ${number}
+            if(!matcher.matches()) {
+                throw new TranslationError("Unexpected token " + tokens[i]);
+            }
+            if(i < 3){ // R-type instruction have exactly 3 register operands
+                int op = Integer.parseInt(tokens[i].substring(1));
+                if(op > 31) {
+                    throw new TranslationError("Register index out of bounds: " + tokens[i]);
+                }
+                operands[i] = op;
+            }
+        }
+        if(tokens.length != 3) { // there is unexpected text after the 3 valid operands
+            throw new TranslationError(
+                    String.format("Instruction %s expects 3 operands. Got %d instead.", instructionId, tokens.length)
+            );
+        }
+        if(operands[0] == 0) { // destination register can't be $0
+            throw new TranslationError("Use of $0 as destination is forbidden");
+        }
+        return (opCode << 26) | (operands[1] << 21) | (operands[2] << 16) | (operands[0] << 11) | (instructionFunc.get(instructionId));
+    }
+
+    private static int parseRShift(String instructionId, int opCode, String body) throws TranslationError {
+        String[] tokens = body.split("\\s*,\\s*"); // split the remaining instruction tokens by ,
+        int[] operands = new int[3];
+        for(int i = 0; i < tokens.length; i++) {
+            Matcher matcher = registerOperandPattern.matcher(tokens[i]); // try to match the token as a valid
+            // operand ${number}
+            if(!matcher.matches()) {
+                throw new TranslationError("Unexpected token " + tokens[i]);
+            }
+            if(i < 2){ // R-type instruction have exactly 3 register operands
+                int op = Integer.parseInt(tokens[i].substring(1));
+                if(op > 31) {
+                    throw new TranslationError("Register index out of bounds: " + tokens[i]);
+                }
+                operands[i] = op;
+            }
+            else if (i == 2){
+                try {
+                    operands[2] = Integer.parseInt(tokens[2]);
+                } catch (NumberFormatException e){
+                    throw new TranslationError("Unexpected token" + tokens[i]);
+                }
+            }
+        }
+        if(tokens.length != 3) { // there is unexpected text after the 3 valid operands
+            throw new TranslationError(
+                    String.format("Instruction %s expects 3 operands. Got %d tokens instead.", instructionId, tokens.length)
+            );
+        }
+        if(operands[0] == 0) { // destination register can't be $0
+            throw new TranslationError("Use of $0 as destination is forbidden");
+        }
+        return (opCode << 26) | (operands[1] << 16) | (operands[0] << 11) | (operands[2] << 6) | (instructionFunc.get(instructionId));
     }
 }
